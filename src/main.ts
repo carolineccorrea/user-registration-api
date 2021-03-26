3dd82f45b9eef3cf326022a20a0eae547ba3e197
Carol <castrocaroline11@gmail.com>
2021-03-26 08:28:22 -0300

    add documentacao

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    // Open API
+    const options = new DocumentBuilder()
+    .setTitle('NESTJS API CRUD - DESAFIO COOPERSYSTEM ')
+    .setDescription('API de usuarios')
+    .setVersion('1.0.0')
+    .addTag('nestjsapi')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document); //  localhost:3000/docs  //
+
   await app.listen(3000);
 }
 bootstrap();
